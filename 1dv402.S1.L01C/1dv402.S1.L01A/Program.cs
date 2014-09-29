b9056dff08ab9e48d29db5ced552a8d42f794a66
Sebastian Fernstedt <sf222em@student.lnu.se>
2014-09-29 17:04:18 +0200

    Method ViewReciept implemented

--- 1dv402.S1.L01C/1dv402.S1.L01A/Program.cs
+++ 1dv402.S1.L01C/1dv402.S1.L01A/Program.cs
@@ -19,51 +19,66 @@ namespace _1dv402.S1.L01C
                 double roundingOffAmount = total - subtotal;
                 uint returnSum = payedSum - total;
 
-                Console.WriteLine("KVITTO\n-----------------------------------------");
-                Console.WriteLine("Totalt: {0:c2}", subtotal);
-                Console.WriteLine("Öresavrundning: {0:c2}", roundingOffAmount);
-                Console.WriteLine("Att betala: {0:c2}", total);
-                Console.WriteLine("Kontant: {0:c2}", payedSum);
-                Console.WriteLine("Tillbaka: {0:c2}\n-----------------------------------------", returnSum);
+                //Console.WriteLine("KVITTO\n-----------------------------------------");
+                //Console.WriteLine("Totalt: {0:c2}", subtotal);
+                //Console.WriteLine("Öresavrundning: {0:c2}", roundingOffAmount);
+                //Console.WriteLine("Att betala: {0:c2}", total);
+                //Console.WriteLine("Kontant: {0:c2}", payedSum);
+                //Console.WriteLine("Tillbaka: {0:c2}\n-----------------------------------------", returnSum);
 
-                SplitIntoDenominations(returnSum);
+                uint[] denominations = { 500, 100, 50, 20, 10, 5, 1 };
+                uint[] notes = SplitIntoDenominations(returnSum, denominations);
+
+                //foreach (uint i in notes)
+                //{
+                //    Console.WriteLine(i);
+                //}
+
+                ViewReceipt(subtotal, roundingOffAmount, total, payedSum, returnSum, notes, denominations);
 
                 ViewMessage("Tryck ny tangent för ny beräkning - Esc avslutar");
 
             } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
         }
-        static void SplitIntoDenominations(uint returnSum)
+        static uint[] SplitIntoDenominations(uint returnSum, uint[] denominations)
         {
             uint count = 0;
-            uint[] values = { 500, 100, 50, 20, 10, 5, 1 };
+            int iterator = 0;
             string form = null; ;
+            uint[] notes = new uint[denominations.GetLength(0)];
             
-            foreach (uint i in values)
+            foreach (uint i in denominations)
             {
                 count = returnSum / i;
                 returnSum -= i * count;
                 returnSum = returnSum % i;
-                if (count > 0)
-                {
-                    switch(i)
-                    {
-                        case 500:
-                        case 100:
-                        case 50:
-                        case 20:
-                            form = "lappar";
-                            break;
-                        case 10:
-                        case 5:
-                        case 1:
-                            form = "kronor";
-                            break;
 
-                    }
+                notes[iterator] = count;
+                iterator++;
+                //if (count > 0)
+                //{
+                //    switch(i)
+                //    {
+                //        case 500:
+                //        case 100:
+                //        case 50:
+                //        case 20:
+                //            form = "lappar";
+                //            break;
+                //        case 10:
+                //        case 5:
+                //        case 1:
+                //            form = "kronor";
+                //            break;
+
+                //    }
                     
-                    Console.WriteLine("{0}-{1}: {2}", i, form, count);
-                }
+
+                    //Console.WriteLine("{0}-{1}: {2}", i, form, count);
+                //}
             }
+
+            return notes;
         }
 
         static double ReadPositiveDouble(string prompt)
@@ -135,8 +150,46 @@ namespace _1dv402.S1.L01C
 
         }
 
-        static void ViewReceipt(double subtotal, double roundingOffAmount, uint total, uint cash, uint change, uint[] notes, uint[] denominations)
+        static void ViewReceipt(double subtotal, double roundingOffAmount, uint total, uint payedSum, uint returnSum, uint[] notes, uint[] denominations)
         {
+            int iterator = 0;
+            string form = null;
+
+            Console.WriteLine("KVITTO\n-----------------------------------------");
+            Console.WriteLine("Totalt: {0:c2}", subtotal);
+            Console.WriteLine("Öresavrundning: {0:c2}", roundingOffAmount);
+            Console.WriteLine("Att betala: {0:c2}", total);
+            Console.WriteLine("Kontant: {0:c2}", payedSum);
+            Console.WriteLine("Tillbaka: {0:c2}\n-----------------------------------------", returnSum);
+
+            foreach (uint i in notes)
+            {
+                if (i > 0)
+                {
+                    uint value = denominations[iterator];
+                    
+                    switch (iterator)
+                    {
+                        case 500:
+                        case 100:
+                        case 50:
+                        case 20:
+                            form = "lappar";
+                            break;
+                        case 10:
+                        case 5:
+                        case 1:
+                            form = "kronor";
+                            break;
+
+                    }
+
+
+                    Console.WriteLine("{0}-{1}: {2}", value, form, i);
+                }
+                iterator++;
+            }
+
 
         }
 
